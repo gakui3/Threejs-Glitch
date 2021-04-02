f6a13fcab6c8b62c0a295d788d91fd4d7b5f2b24
gKirii <gaku.kirii@gmail.com>
2021-04-02 12:04:08 +0900

    Change ts to js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'development',
+  mode: "development",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: './docs/main.ts',
+  entry: "./docs/main.js",
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
     path: `${__dirname}/docs`,
     // 出力ファイル名
-    filename: 'main.js',
+    filename: "dest.js",
   },
   module: {
     rules: [
@@ -18,19 +18,19 @@ module.exports = {
         // 拡張子 .ts の場合
         test: /\.ts$/,
         // TypeScript をコンパイルする
-        use: 'ts-loader',
+        use: "ts-loader",
       },
       {
         test: /\.(glsl|vs|fs|vert|frag)$/,
         exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
+        use: ["raw-loader", "glslify-loader"],
       },
     ],
   },
   // import 文で .ts ファイルを解決するため
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   // ES5(IE11等)向けの指定（webpack 5以上で必要）
-  target: ['web', 'es5'],
+  target: ["web", "es5"],
 };
